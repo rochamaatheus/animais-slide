bad2c37ce043886533536ccb6504f050b6bc97d6
Matheus <matheussilveirarocha.sc@gmail.com>
2022-12-05 08:48:24 -0300

    Transição Suave

--- js/slide.js
+++ js/slide.js
@@ -16,6 +16,7 @@ export default class Slide {
       movetype = 'touchmove';
     }
     this.wrapper.addEventListener(movetype, this.onMove);
+    this.transiton(false);
   }
 
   onMove(event) {
@@ -32,6 +33,10 @@ export default class Slide {
     return this.dist.finalPosition - this.dist.movement;
   }
 
+  transiton(active) {
+    this.slide.style.transition = active ? 'transform .3s' : '';
+  }
+
   moveSlide(distX) {
     this.dist.movePosition = distX;
     this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
@@ -41,6 +46,18 @@ export default class Slide {
     const moveType = event.type === 'mouseup' ? 'mousemove' : 'touchmove';
     this.wrapper.removeEventListener(moveType, this.onMove);
     this.dist.finalPosition = this.dist.movePosition;
+    this.transiton(true);
+    this.changeSlideOnEnd();
+  }
+
+  changeSlideOnEnd() {
+    if (this.dist.movement > 120 && this.index.next !== undefined) {
+      this.activeNextSlide();
+    } else if (this.dist.movement < -120 && this.index.prev !== undefined) {
+      this.activePrevSlide();
+    } else {
+      this.changeSlide(this.index.active);
+    }
   }
 
   bindEvents() {
@@ -87,11 +104,23 @@ export default class Slide {
     this.dist.finalPosition = activeSlide.position;
   }
 
+  activePrevSlide() {
+    if (this.index.prev !== undefined) {
+      this.changeSlide(this.index.prev);
+    }
+  }
+
+  activeNextSlide() {
+    if (this.index.next !== undefined) {
+      this.changeSlide(this.index.next);
+    }
+  }
+
   init() {
     this.bindEvents();
+    this.transiton(true);
     this.addSlideEvents();
     this.slidesConfig();
-    this.changeSlide(2);
     return this;
   }
 }
